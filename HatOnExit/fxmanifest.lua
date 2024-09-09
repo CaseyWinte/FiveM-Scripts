39d2ec6e74711f546a7d7eb8cbc0d891d8443ca0
CaseyWinte <100818551+CaseyWinte@users.noreply.github.com>
2024-09-09 01:33:47 -0400

    Add files via upload

---
+++ HatOnExit/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+
+author 'Delta Development'
+description 'Put on a hat animation when exiting an emergency vehicle'
+version '1.0.0'
+
+client_script 'client.lua'
