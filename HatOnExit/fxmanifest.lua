7e245a4786fed2bb4bf30763a9e1a4081f2b3c7d
CaseyWinte <100818551+CaseyWinte@users.noreply.github.com>
2024-09-10 18:51:54 -0400

    Update fxmanifest.lua
    removed lua54 and updated author

--- HatOnExit/fxmanifest.lua
+++ HatOnExit/fxmanifest.lua
@@ -1,9 +1,8 @@
 fx_version 'cerulean'
 game 'gta5'
 
-lua54 'yes'
+author 'Casey Winter / Delta Development'
 
-author 'Delta Development'
 description 'Put on a hat animation when exiting an emergency vehicle'
 version '1.0.0'
 
