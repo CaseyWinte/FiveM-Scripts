76e7e73925e47b71cd44aed8e7d3a2635178b34d
CaseyWinte <100818551+CaseyWinte@users.noreply.github.com>
2024-09-10 20:02:59 -0400

    Create config.lua

---
+++ HatOnExit/config.lua
@@ -0,0 +1,13 @@
+Config = {}
+
+-- List of vehicle models that the script works for
+-- You can add any vehicle model name here
+Config.AllowedVehicles = {
+    "justice1",
+    "police2",
+    "police3",
+    "police4",
+    "firetruk",
+    "ambulance",
+    -- Add more vehicles as needed
+}
